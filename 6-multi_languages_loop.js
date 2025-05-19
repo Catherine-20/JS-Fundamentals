fba573e1caaa890abd94b4da872342a2ed9837ee
Catherine-20 <catherineingabire15@gmail.com>
2025-05-19 15:26:56 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -2,10 +2,8 @@ const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 let output = "";
 
 for (let i = 0; i < lines.length; i++) {
-  output += lines[i];
-  if (i < lines.length - 1) {
-    output += "\n";
-  }
+  output += lines[i] + (i === lines.length - 1 ? "" : "\n");
 }
 
 console.log(output);
+
